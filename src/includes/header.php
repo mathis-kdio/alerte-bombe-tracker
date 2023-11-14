f6004543a8e8ed811eee496657595adc338cfe59
Mathis Cadio <cadio@et.esiea.fr>
2023-11-14 22:41:28 +0100

    Init

---
+++ src/includes/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="fr" class="h-100">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Alerte Bombe Tracker</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="includes/includes.css" rel="stylesheet">
+  </head>
+  <body class="d-flex flex-column h-100">
+    <div class="container-fluid">
+      <header class="d-flex flex-wrap justify-content-center py-3 border-bottom">
+        <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
+          <span class="fs-4">Alerte Bombe Tracker</span>
+        </a>
+
+        <ul class="nav nav-pills">
+          <li class="nav-item"><a href="/" class="nav-link">Accueil</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">France</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Départements</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">Villes</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">À propos</a></li>
+        </ul>
+      </header>
+    </div>
+    <div class="alert alert-danger text-center" role="alert">
+      Article 322-14 du Code pénal : « Le fait de communiquer ou de divulguer une fausse information dans le but de faire croire qu’une destruction, une dégradation ou une détérioration dangereuse pour les personnes va être ou a été commise est puni de deux ans d’emprisonnement et de 30 000 € d’amende. »
+    </div>
---
+++ src/index.php
@@ -0,0 +1,40 @@
+<?php
+include_once("includes/header.php");
+?>
+
+<main class="flex-shrink-0">
+  <div class="container">
+    <h1 class="mt-5">Statistiques et visualisations des alertes à la bombe en France</h1>
+    <p class="lead">AlerteBombeTracker est un outil permettant de suivre l'évolution des alertes à la bombe en France. Pour des analyses quotidiennes des chiffres, vous pouvez suivre @ sur X.</p>
+    
+    <div class="row align-items-md-stretch">
+      <div class="col-md-12">
+        <div class="h-100 p-5 bg-body-tertiary border rounded-3">
+          <h2>Les chiffres d'aujoud'hui</h2>
+          <div class="row">
+            <div class="col-md-3">
+              <h3>XXX</h3>
+              <h4>Alertes à la bombe</h4>
+            </div>
+            <div class="col-md-3">
+              <h3>XXX</h3>
+              <h4>Nombre de villes impactées</h4>
+            </div>
+            <div class="col-md-3">
+              <h3>XXX</h3>
+              <h4>Etablissements scolaires impactés</h4>
+            </div>
+            <div class="col-md-3">
+              <h3>XXX</h3>
+              <h4>Arrestations</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
+
+<?php
+include_once("includes/footer.php");
+?>
