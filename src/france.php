559673d1f5414f5ed4d9d6c8b2e1df8b82d8ce43
Mathis Cadio <cadio@et.esiea.fr>
2023-11-14 23:21:15 +0100

    Page France

--- src/france.php
+++ src/france.php
@@ -4,30 +4,36 @@ include_once("includes/header.php");
 
 <main class="flex-shrink-0">
   <div class="container">
-    <h1 class="mt-5">Statistiques et visualisations des alertes à la bombe en France</h1>
-    <p class="lead">AlerteBombeTracker est un outil permettant de suivre l'évolution des alertes à la bombe en France. Pour des analyses quotidiennes des chiffres, vous pouvez suivre @ sur X.</p>
+    <h1 class="mt-5">Récapitatif pour la France</h1>
     
     <div class="row align-items-md-stretch">
       <div class="col-md-12">
-        <div class="h-100 p-5 bg-body-tertiary border rounded-3 mb-3">
-          <h2>Les chiffres d'aujoud'hui (14-11-2023)</h2>
+        <div class="p-5 bg-body-tertiary border rounded-3 mb-3">
+          <h2>Au total</h2>
           <div class="row">
             <div class="col-md-3">
-              <h3>21</h3>
+              <h3>XXX</h3>
               <h4>Alertes à la bombe</h4>
             </div>
             <div class="col-md-3">
-              <h3>7</h3>
+              <h3>XXX</h3>
               <h4>Nombre de villes impactées</h4>
             </div>
             <div class="col-md-3">
-              <h3>21</h3>
+              <h3>XXX</h3>
               <h4>Etablissements scolaires impactés</h4>
             </div>
             <div class="col-md-3">
               <h3>XXX</h3>
               <h4>Arrestations</h4>
             </div>
+          </div>
+        </div>
+
+        <div class="p-5 bg-body-tertiary border rounded-3 mb-3">
+          <h2>Nombre d'alertes à la bombe chaque jour</h2>
+          <div class="row">
+
           </div>
         </div>
       </div>
